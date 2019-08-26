bb9f51029f8bb410c18c03b34da289733ed4ec36
autumn-ragland <raglanda998@gmail.com>
2019-08-26 13:45:12 -0500

    Graded 08-26-2019 13:44:48

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # python-functions-cw_2
 
+Good work. Score : 5. See index.py for comments
 
 ### Problem 1:
 Create a function that will ask the user for a number. Use the function to get two numbers from the user, then pass the two numbers to a function. Add, subtract, multiple, and divide the numbers.
--- index.py
+++ index.py
@@ -5,12 +5,12 @@
 # then pass the two numbers to a function. Add, subtract, multiple, and divide the numbers.
 
 def twoNum():
-    num1 = int(input('enter number'))
+    num1 = int(input('enter number')) # !! : add some formatting (space after string)
     num2 = int(input('enter number'))
     add(num1, num2)
 
 
-def add(num1, num2):
+def add(num1, num2): # !! : not a great function name
     print(num1 + num2)
     print(num1 - num2)
     print(num1 * num2)
